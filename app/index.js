2c4a2586231a4840c2d4493f3ed364f1ee1cdee5
Marco Bruno <marco.bruno.br@gmail.com>
2018-10-15 04:19:50 +0000

    Usando a base que o thebilyto fez pra gente

--- app/index.js
+++ app/index.js
@@ -1,35 +1,36 @@
-import Phaser from 'Phaser'
-var config = {
+import Phaser from "phaser";
+
+const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
     physics: {
-        default: 'arcade',
+        default: "arcade",
         arcade: {
-            gravity: { y: 200 },
-        },
+            gravity: { y: 200 }
+        }
     },
     scene: {
         preload: preload,
-        create: create,
-    },
+        create: create
+    }
 };
 var game = new Phaser.Game(config);
 function preload() {
-    this.load.setBaseURL('http://labs.phaser.io');
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+    this.load.setBaseURL("http://labs.phaser.io");
+    this.load.image("sky", "assets/skies/space3.png");
+    this.load.image("logo", "assets/sprites/phaser3-logo.png");
+    this.load.image("red", "assets/particles/red.png");
 }
 function create() {
-    this.add.image(400, 300, 'sky');
-    var particles = this.add.particles('red');
+    this.add.image(400, 300, "sky");
+    var particles = this.add.particles("red");
     var emitter = particles.createEmitter({
         speed: 100,
         scale: { start: 1, end: 0 },
-        blendMode: 'ADD',
+        blendMode: "ADD"
     });
-    var logo = this.physics.add.image(400, 100, 'logo');
+    var logo = this.physics.add.image(400, 100, "logo");
     logo.setVelocity(100, 200);
     logo.setBounce(1, 1);
     logo.setCollideWorldBounds(true);
